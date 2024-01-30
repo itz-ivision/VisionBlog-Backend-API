89e1dc5229130314e925d4f7d08e8e18ebfa38fe
Arnab <arnab.ar7@gmail.com>
2024-01-30 23:29:37 +0530

    added new routes and added PUT method in app-js

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
-import { addPost, deletePost, getAllPosts } from "../controllers/post.controllers.js";
+import { addPost, deletePost, getAllPosts, getPostByID,updatePost } from "../controllers/post.controllers.js";
 
 
 const router = Router()
 
 router.route("/")
     .post(addPost)
-    .get(getAllPosts)
+    .get(getAllPosts);
     
-router.route("/:postId").delete(deletePost);
+router.route("/:postId")
+    .get(getPostByID)
+    .put(updatePost)
+    .delete(deletePost);
+
 
 export default router
